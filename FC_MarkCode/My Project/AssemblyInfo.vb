dd962d1f87392923b164fc728a552deee565588b
Zulhisham Tan <zultan@pinc.my>
2025-02-18 10:19:08 +0800

    solve repo issue

--- FC_MarkCode/My Project/AssemblyInfo.vb
+++ FC_MarkCode/My Project/AssemblyInfo.vb
@@ -33,7 +33,7 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("2023.01.10.013")>
-<Assembly: AssemblyFileVersion("2023.01.10.013")>
+<Assembly: AssemblyVersion("2025.02.18.014")>
+<Assembly: AssemblyFileVersion("2025.02.18.014")>
 
 <Assembly: NeutralResourcesLanguageAttribute("en-GB")> 
